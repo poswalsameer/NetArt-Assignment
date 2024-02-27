eba5877cb3b0632e4ac808f987ac8a86ae46d001
poswalsameer <poswalsameer2003@gmail.com>
2024-02-27 23:55:37 +0530

    almost done

--- netart/src/App.jsx
+++ netart/src/App.jsx
@@ -5,51 +5,284 @@ function App() {
 
   return (
     <>
-     <div className=' h-full w-screen flex flex-col justify-center items-center bg-gradient-to-b from-slate-100 to-yellow-200'>
+     <div className='h-full w-full flex flex-col justify-center items-center bg-gradient-to-b from-slate-100 to-yellow-200
+     
+     sm:h-full sm:w-full sm:flex sm:flex-col sm:justify-center sm:items-center sm:bg-gradient-to-b sm:from-slate-100 sm:to-yellow-200
+     
+     md:h-full md:w-full md:flex md:flex-col md:justify-center md:items-center md:bg-gradient-to-b md:from-slate-100 md:to-yellow-200
+     
+     lg:h-full lg:w-full lg:flex lg:flex-col lg:justify-center lg:items-center lg:bg-gradient-to-b lg:from-slate-100 lg:to-yellow-200
+     
+     xl:h-full xl:w-full xl:flex xl:flex-col xl:justify-center xl:items-center xl:bg-gradient-to-b xl:from-slate-100 xl:to-yellow-200
+     
+     2xl:h-full 2xl:w-full 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center 2xl:bg-gradient-to-b 2xl:from-slate-100 2xl:to-yellow-200'>
 
-        <img src="logo.png" alt="" className='h-32 w-96'/>
+        <img src="logo.png" alt="" className='h-32 w-96
+        
+        sm:h-32 sm:w-96
+        
+        md:h-32 md:w-96
+        
+        lg:h-32 lg:w-96
+        
+        xl:h-32 xl:w-96 
+        
+        2xl:h-32 2xl:w-96'/>
 
-        <div className='flex flex-row justify-center items-center'>
+        <div className='flex flex-row my-10 justify-evenly items-center text-xs
+        
+        sm:flex sm:flex-row sm:my-10 sm:justify-between sm:items-center sm:text-xs
 
-            <img src="1.png" alt="" className='h-96 w-52'/>
+        
+        md:flex md:flex-row md:my-10 md:justify-between md:items-center
+        
+        lg:flex lg:flex-row lg:my-10 lg:justify-between lg:items-center
+        
+        xl:flex xl:flex-row xl:my-10 xl:justify-between xl:items-center
+        
+        2xl:flex 2xl:flex-row 2xl:my-10 2xl:justify-between 2xl:items-center'>
 
-            <div className=' w-[60rem] flex flex-col justify-center items-center '>
+            <img src="1.png" alt="" className='h-[15rem] w-32 mr-14 
+            
+            sm:h-[35rem] sm:w-64 sm:mr-14 
+            
+            md:h-[35rem] md:w-64 md:mr-14
+            
+            lg:h-[32rem] lg:w-80 lg:mr-14
+            
+            xl:h-[38rem] xl:w-96 xl:mr-14
+            
+            2xl:h-[38rem] 2xl:w-96 2xl:mr-20 '/>
 
-              <h1 className='font-extrabold'>
+            <div className='w-[18rem] flex flex-col justify-center items-center mr-1 
+            
+            sm:w-[35rem] sm:flex sm:flex-col sm:justify-center sm:items-center sm:mr-3
+            
+            md:w-[44rem] md:flex md:flex-col md:justify-center md:items-center md:mr-3
+            
+            lg:w-[60rem] lg:flex lg:flex-col lg:justify-center lg:items-center lg:mr-3
+            
+            xl:w-[60rem] xl:flex xl:flex-col xl:justify-center xl:items-center 
+            
+            2xl:w-[60rem] 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-center '>
+
+              <h1 className='font-extrabold sm:font-extrabold md:font-extrabold lg:font-extrabold xl:font-extrabold 2xl:font-extrabold '>
                 C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.
               </h1>
 
               <ul>
-                <li className='list-disc my-5'>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
-                <li className='list-disc my-5'>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
+                <li className='list-disc my-5 font-bold
+                
+                sm:list-disc sm:my-5 sm:font-bold
+                
+                md:list-disc md:my-5 md:font-bold
+                
+                lg:list-disc lg:my-5 lg:font-bold
+                
+                xl:list-disc xl:my-5 xl:font-bold
+                
+                2xl:list-disc 2xl:my-5 2xl:font-bold'>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+
+                <li className='list-disc my-5 font-bold
+                
+                sm:list-disc sm:my-5 sm:font-bold
+                
+                md:list-disc md:my-5 md:font-bold
+                
+                lg:list-disc lg:my-5 lg:font-bold
+                
+                xl:list-disc xl:my-5 xl:font-bold
+                
+                2xl:list-disc 2xl:my-5 2xl:font-bold'>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
               </ul>
 
-              <img src="2.png" alt="" className='h-96 w-full' />
+              <img src="2.png" alt="" className='h-52 w-full
+              
+              sm:h-80 sm:w-full
+              
+              md:h-80 md:w-full lg:h-80 lg:w-full 
+              
+              xl:h-96 xl:w-full 2xl:h-96 2xl:w-full' />
 
-              <h1>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</h1>
+              <h1 className='my-5 font-semibold
+              
+              sm:my-5 sm:font-semibold
+              
+              md:my-5 md:font-semibold
+              
+              lg:my-5 lg:font-semibold xl:my-5 xl:font-semibold 2xl:my-5 2xl:font-semibold'>Government of India has awarded the <span className='font-extrabold'>"National Energy Conservation Award 2018"</span>. Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</h1>
 
 
             </div>          
 
         </div>
 
-        <h1>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</h1>
+        <h1 className=' mx-2 my-5 font-bold text-center
+        
+        sm:my-5 sm:font-bold sm:text-center
+        
+        md:my-5 md:font-bold md:text-center
+        
+        lg:my-5 lg:font-bold lg:text-center
+        
+        xl:my-5 xl:font-bold xl:text-center
+        
+        2xl:my-5 2xl:font-bold 2xl:text-center'>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</h1>
 
-        <img src="3.png" alt="" className='w-[85%]'/>
+        <img src="3.png" alt="" className='w-[85%] my-10
+        
+        sm:w-[85%] sm:my-10
+        
+        md:w-[85%] md:my-10
+        
+        lg:w-[85%] lg:my-10
+        
+        xl:w-[85%] xl:my-10
+        
+        2xl:w-[85%] 2xl:my-10'/>
 
-        <h1>
+        <h1 className='mx-2 text-center font-bold my-5
+        
+        sm:font-bold sm:my-5
+        
+        md:font-bold md:my-5
+        
+        lg:font-bold lg:my-5
+        
+        xl:font-bold xl:my-5
+        
+        2xl:font-bold 2xl:my-5'>
           Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors
         </h1>
 
-        <div className='h-1 w-[90%] bg-red-600' ></div>
+        <div className='h-1 w-[90%] bg-red-600
+        
+        sm:h-1 sm:w-[90%] sm:bg-red-600 
+        
+        md:h-1 md:w-[90%] md:bg-red-600        
+        
+        lg:h-1 lg:w-[90%] lg:bg-red-600
+        
+        xl:h-1 xl:w-[90%] xl:bg-red-600
+        
+        2xl:h-1 2xl:w-[90%] 2xl:bg-red-600' ></div>
 
-        <h1>
+        <h1 className=' text-center my-5 font-extrabold
+        
+        sm:my-5 sm:font-extrabold
+        
+        md:my-5 md:font-extrabold
+        
+        lg:my-5 lg:font-extrabold
+        
+        xl:my-5 xl:font-extrabold
+        
+        2xl:my-5 2xl:font-extrabold'>
           C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS
         </h1>
 
-        <h1>
-        CHEMICALS & PROCESS POWER WATER & WASTE WATER OILS & GAS PHARMA SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING FOOD & BEVERAGE PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE & RESIDENTIAL
+        <h1 className='font-bold text-center w-[97%] my-5
+        
+        sm:font-bold sm:text-center sm:w-[97%] sm:my-5
+        
+        md:font-bold md:text-center md:w-[97%] md:my-5
+        
+        lg:font-bold lg:text-center lg:w-[97%] lg:my-5
+        
+        xl:font-bold xl:text-center xl:w-[97%] xl:my-5
+        
+        2xl:font-bold 2xl:text-center 2xl:w-[97%] 2xl:my-5'>
+
+        CHEMICALS <span className='font-extrabold text-red-600'>|</span> PROCESS POWER WATER <span className='font-extrabold text-red-600'>|</span> WASTE WATER OILS <span className='font-extrabold text-red-600'>|</span> GAS PHARMA SUGARS <span className='font-extrabold text-red-600'>|</span> DISTILLERIES PAPER <span className='font-extrabold text-red-600'>|</span> PULP MARINE <span className='font-extrabold text-red-600'>|</span> DEFENCE METAL <span className='font-extrabold text-red-600'>|</span> MINING FOOD <span className='font-extrabold text-red-600'>|</span> BEVERAGE PETROCHEMICAL <span className='font-extrabold text-red-600'>|</span> REFINERIES <span className='font-extrabold text-red-600'>|</span> SOLAR <span className='font-extrabold text-red-600'>|</span> BUILDING <span className='font-extrabold text-red-600'>|</span> HVAC <span className='font-extrabold text-red-600'>|</span> FIRE FIGHTING <span className='font-extrabold text-red-600'>|</span> AGRICULTURE <span className='font-extrabold text-red-600'>|</span> RESIDENTIAL
         </h1>
+
+        <div className='h-40 w-[97%] bg-red-600 my-3 grid grid-cols-3 place-content-center items-center
+        
+        sm:h-40 sm:w-[97%] sm:bg-red-600 sm:my-3 sm:grid sm:grid-cols-3 sm:place-content-center sm:items-center
+        
+        md:h-40 md:w-[97%] md:bg-red-600 md:my-3 md:grid md:grid-cols-3 md:place-content-center md:items-center
+        
+        lg:h-40 lg:w-[97%] lg:bg-red-600 lg:my-3 lg:grid lg:grid-cols-3 lg:place-content-center lg:items-center
+        
+        xl:h-40 xl:w-[97%] xl:bg-red-600 xl:my-3 xl:grid xl:grid-cols-3 xl:place-content-center-center xl:items-center
+        
+        2xl:h-40 2xl:w-[97%] 2xl:bg-red-600 2xl:my-3 2xl:grid 2xl:grid-cols-3 2xl:place-content-center 2xl:items-center'>
+
+            <div className='mx-2 text-xs flex flex-col justify-center items-center text-white font-bold
+            
+            sm:mx-2 sm:text-xs sm:flex sm:flex-col sm:justify-center sm:items-center sm:text-white sm:font-bold
+            
+            md:mx-4 md:flex md:flex-col md:justify-center md:items-center md:text-white md:font-bold
+            
+            lg:mx-12 lg:flex lg:flex-row lg:justify-center lg:items-center lg:text-white lg:font-bold 
+            
+            xl:mx-12 xl:flex xl:flex-row xl:justify-center xl:items-center xl:text-white xl:font-bold 
+            
+            2xl:mx-12 2xl:flex 2xl:flex-row 2xl:justify-center 2xl:items-center 2xl:text-white 2xl:font-bold '>
+
+              <img src="call.png" alt="" className='h-10 w-10 my-2
+              
+              sm:h-10 sm:w-10 sm:my-2
+              
+              md:h-10 md:w-10 md:my-2
+              
+              lg:h-10 lg:w-10 lg:mx-2
+              
+              xl:h-10 xl:w-10 xl:mx-2
+              
+              2xl:h-10 2xl:w-10 2xl:mx-2'/>
+
+              <h1>Toll Free 1800 200 1234</h1>
+
+            </div>
+
+            <div className='sm:mx-2 sm:text-xs sm:flex sm:flex-col sm:justify-center sm:items-center sm:text-white sm:font-bold
+            
+            md:mx-5 md:flex md:flex-col md:justify-center md:items-center md:text-white md:font-bold
+            
+            lg:mx-28 lg:flex lg:flex-row lg:justify-center lg:items-center lg:text-white lg:font-bold
+            
+            xl:mx-28 xl:flex xl:flex-row xl:justify-center xl:items-center xl:text-white xl:font-bold
+            
+            2xl:mx-28 2xl:flex 2xl:flex-row 2xl:justify-center 2xl:items-center 2xl:text-white 2xl:font-bold'>
+
+              <img src="facebook.jpg" alt="" className='sm:h-10 sm:w-10 sm:my-2
+              
+              md:h-10 md:w-10 md:my-2
+              
+              lg:h-10 lg:w-10 lg:mx-2
+              
+              xl:h-10 xl:w-10 xl:mx-2
+              
+              2xl:h-10 2xl:w-10 2xl:mx-2'/>
+              <h1>www.facebook.com/cripumps</h1>
+            </div>
+
+            <div className='sm:mx-12 sm:text-xs sm:flex sm:flex-col sm:justify-center sm:items-center sm:text-white sm:font-bold
+            
+            md:mx-12 md:flex md:flex-col md:justify-center md:items-center md:text-white md:font-bold
+            
+            lg:mx-12 lg:flex lg:flex-row lg:justify-center lg:items-center lg:text-white lg:font-bold
+            
+            xl:mx-12 xl:flex xl:flex-row xl:justify-center xl:items-center xl:text-white xl:font-bold
+            
+            2xl:mx-12 2xl:flex 2xl:flex-row 2xl:justify-center 2xl:items-center 2xl:text-white 2xl:font-bold'>
+
+              <img src="globe.png" alt="" className='sm:h-12 sm:w-12 sm:mx-2
+              
+              md:h-12 md:w-12 md:mx-2
+              
+              lg:h-10 lg:w-10 lg:mx-2
+              
+              xl:h-10 xl:w-10 xl:mx-2
+              
+              2xl:h-16 2xl:w-16 2xl:mx-2'/>
+
+              <h1>www.crigroups.com</h1>
+
+            </div>
+
+        </div>
 
      </div>
     </>
