37fed01112a738e9943f5e4a7db85ba22fe4ad64
poswalsameer <poswalsameer2003@gmail.com>
2024-02-27 14:16:14 +0530

    skeleton is ready

---
+++ netart/src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className=' h-full w-screen flex flex-col justify-center items-center bg-gradient-to-b from-slate-100 to-yellow-200'>
+
+        <img src="logo.png" alt="" className='h-32 w-96'/>
+
+        <div className='flex flex-row justify-center items-center'>
+
+            <img src="1.png" alt="" className='h-96 w-52'/>
+
+            <div className=' w-[60rem] flex flex-col justify-center items-center '>
+
+              <h1 className='font-extrabold'>
+                C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.
+              </h1>
+
+              <ul>
+                <li className='list-disc my-5'>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+                <li className='list-disc my-5'>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel.</li>
+              </ul>
+
+              <img src="2.png" alt="" className='h-96 w-full' />
+
+              <h1>Government of India has awarded the "National Energy Conservation Award 2018". Mr. G. Selvaraj, Joint Managing Director of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable Minister of State.</h1>
+
+
+            </div>          
+
+        </div>
+
+        <h1>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</h1>
+
+        <img src="3.png" alt="" className='w-[85%]'/>
+
+        <h1>
+          Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors
+        </h1>
+
+        <div className='h-1 w-[90%] bg-red-600' ></div>
+
+        <h1>
+          C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS
+        </h1>
+
+        <h1>
+        CHEMICALS & PROCESS POWER WATER & WASTE WATER OILS & GAS PHARMA SUGARS & DISTILLERIES PAPER & PULP MARINE & DEFENCE METAL & MINING FOOD & BEVERAGE PETROCHEMICAL & REFINERIES SOLAR BUILDING HVAC FIRE FIGHTING AGRICULTURE & RESIDENTIAL
+        </h1>
+
+     </div>
+    </>
+  )
+}
+
+export default App
